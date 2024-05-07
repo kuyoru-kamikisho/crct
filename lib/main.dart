2b63404b16e20f6088262371f0819fe198d639be
k <k@k.com>
2024-05-07 16:50:46 +0800

    窗口样式变更与制限

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,25 @@
 import 'package:feriea/public_states.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
 import 'my_home_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await windowManager.ensureInitialized();
+
+  WindowOptions windowOptions = const WindowOptions(
+      size: Size(800, 600),
+      center: true,
+      backgroundColor: Colors.transparent,
+      skipTaskbar: false,
+      titleBarStyle: TitleBarStyle.normal,
+      windowButtonVisibility: false,
+      minimumSize: Size(338, 546));
+  windowManager.waitUntilReadyToShow(windowOptions, () async {
+    await windowManager.show();
+    await windowManager.focus();
+  });
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider(create: (_) {
       var uc = UseCurrency();
