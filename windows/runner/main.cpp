304536eb59e7c6b590d456f612a2602a2744def9
k <k@k.com>
2024-05-08 17:54:21 +0800

    初始化源码

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(617, 664);
-  if (!window.Create(L"Three thoughts methods", origin, size)) {
+  if (!window.Create(L"Learn flutter", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
