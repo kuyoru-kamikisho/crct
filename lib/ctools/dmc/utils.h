3e71f22af7738ec91242a4066b266c32cb58eec5
k <k@k.com>
2024-06-19 15:32:59 +0800

    c++应用程序转为dll库

--- lib/ctools/dmc/utils.h
+++ lib/ctools/dmc/utils.h
@@ -1,5 +1,10 @@
 #pragma once
 #pragma comment(lib, "pdh.lib")
+#ifdef DMC_EXPORTS
+#define DMC_API __declspec(dllexport ) 
+#else
+#define DMC_API __declspec(dllimport)
+#endif
 
 #define BUFFER_SIZE 4096
 
@@ -7,14 +12,14 @@
 /// 获取CPU使用率
 /// </summary>
 /// <param name="milliseconds">CPU时钟记录时间</param>
-float GetCpuUsage(int milliseconds);
+extern"C" DMC_API float GetCpuUsage(int milliseconds);
 /// <summary>
 /// 获取磁盘利用率
 /// </summary>
-float GetDiskUsage();
+extern"C" DMC_API float GetDiskUsage();
 /// <summary>
 /// 获取内存使用率
 /// </summary>
-float GetMemoryUsage();
+extern"C" DMC_API float GetMemoryUsage();
 
 
