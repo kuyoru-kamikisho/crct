174dd1ef9fb400bbf41111eeef7a465a8d011a46
k <k@k.com>
2024-06-20 18:11:38 +0800

    去除冗余的全局变量，修正星期映射

--- lib/store/use_app.dart
+++ lib/store/use_app.dart
@@ -1,7 +1,5 @@
-import 'dart:async';
 import 'dart:isolate';
 import 'package:crct/tools/dmc.dart';
-import 'package:crct/tools/leaf.dart';
 import 'package:flutter/foundation.dart';
 
 class UseApp with ChangeNotifier, DiagnosticableTreeMixin {
@@ -12,25 +10,11 @@ class UseApp with ChangeNotifier, DiagnosticableTreeMixin {
   String _cpu = '0%';
   String _dsk = '0%';
   String _mem = '0%';
-  String _year = '_';
-  String _month = '_';
-  String _day = '_';
-  String _hour = '_';
-  String _minute = '_';
-  String _second = '_';
-  String _week = '_';
 
   String get version => _version;
   String get cpu => _cpu;
   String get disk => _dsk;
   String get memory => _mem;
-  String get year => _year;
-  String get month => _month;
-  String get day => _day;
-  String get hour => _hour;
-  String get minute => _minute;
-  String get second => _second;
-  String get week => _week;
 
   void watchDevicePerformance() {
     if (_isWatchingDevicePerformance) {
@@ -47,19 +31,5 @@ class UseApp with ChangeNotifier, DiagnosticableTreeMixin {
     });
     var dmc = DmcClass(mainPort1: _receivePort.sendPort);
     dmc.spawn();
-  }
-
-  void watchTime() async {
-    Timer.periodic(const Duration(seconds: 1), (timer) {
-      DateTime now = DateTime.now();
-      _year = '${now.year}';
-      _month = '${now.month}';
-      _day = '${now.day}';
-      _hour = padZero(now.hour);
-      _minute = padZero(now.minute);
-      _second = padZero(now.second);
-      _week = jpWeekdays[now.weekday]!;
-      notifyListeners();
-    });
   }
 }
