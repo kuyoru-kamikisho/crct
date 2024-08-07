adf6babf463935c3f5bd8dbbd9bb95a2820e5402
k <k@k.com>
2024-08-07 14:12:32 +0800

    win状态类

---
+++ lib/store/use_win.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/foundation.dart';
+
+class UseWin with ChangeNotifier, DiagnosticableTreeMixin {
+  double _x = 10;
+  double _y = 10;
+  double _w = 256;
+  double _h = 400;
+
+  double get width => _w;
+  double get height => _h;
+  double get x => _x;
+  double get y => _y;
+
+  void winSizeChangeTo(double? w, double? h) {
+    _w = w ?? _w;
+    _h = h ?? _h;
+  }
+
+  void winPositionChangeTo(double? x, double? y) {
+    _x = x ?? _x;
+    _y = y ?? _y;
+    notifyListeners();
+  }
+}
