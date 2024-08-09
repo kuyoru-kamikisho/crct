f7fbfbea8cd044f044324ca7270d30ac0bac5192
k <k@k.com>
2024-08-09 16:55:49 +0800

    unfixed: stdin阻塞服务的问题

--- server/bin/server.dart
+++ server/bin/server.dart
@@ -3,14 +3,14 @@ import 'dart:io';
 import 'package:server/info.dart';
 import 'package:server/rest_register.dart';
 
-void main(List<String> arguments) async {
+void main(List<String> arguments) {
   appWelcome();
-  var server = await restRegister();
-
-  print('【q】关闭服务并退出应用程序');
-  var input = stdin.readLineSync();
-  if (input == 'q') {
-    server.close();
-    exit(0);
-  }
+  restRegister().then((server) {
+    print('【q】关闭服务并退出应用程序');
+    var input = stdin.readLineSync();
+    if (input == 'q') {
+      server.close();
+      exit(0);
+    }
+  });
 }
--- server/test/server_test.dart
+++ server/test/server_test.dart
@@ -1,8 +1,20 @@
+import 'package:http/http.dart' as http;
 import 'package:server/info.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('app_welcome', () {
-    expect(appWelcome(), true);
+  // test('app_welcome', () {
+  //   expect(appWelcome(), true);
+  // });
+
+  test('rest_api_check', () async {
+    final u1 = Uri.parse('http://localhost:8080/');
+    final u2 = Uri.parse('http://localhost:8080/genId');
+
+    final r1 = await http.get(u1);
+    final r2 = await http.get(u2);
+
+    expect(r1.body.isNotEmpty, true);
+    expect(r2.body.isNotEmpty, true);
   });
 }
