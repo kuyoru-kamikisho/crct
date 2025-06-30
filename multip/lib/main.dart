79728856734615d8f59af395fbd436ff31d04d89
kty <yoail@example.com>
2025-06-30 12:35:34 +0800

    url

--- multip/lib/main.dart
+++ multip/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
 import 'package:go_router/go_router.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 void main() {
   runApp(MyApp());
@@ -185,9 +186,27 @@ class _TextSearcherState extends State<TextSearcher> {
     });
   }
 
+  Future<void> doSearch() async {
+    var text = searchController.text.trim();
+    if (text.isEmpty) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('搜索内容不可以为空')));
+      return;
+    }
+
+    var targetUrl = Uri.parse(getActive().target + text);
+    if (!await launchUrl(targetUrl, mode: LaunchMode.externalApplication)) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('无法打开此链接')));
+    }
+  }
+
   @override
   void dispose() {
     searchController.dispose();
+    focusNode.dispose();
     super.dispose();
   }
 
@@ -199,108 +218,128 @@ class _TextSearcherState extends State<TextSearcher> {
         data: TextSelectionThemeData(
           selectionColor: Color.fromARGB(255, 63, 40, 147),
         ),
-        child: Container(
-          padding: EdgeInsets.all(0),
-          decoration: BoxDecoration(
-            border: Border.all(),
-            color: Color.fromARGB(40, 0, 0, 0),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 420,
-                child: TextField(
-                  focusNode: focusNode,
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: WidgetStateColor.resolveWith((states) {
-                      return states.contains(WidgetState.hovered)
-                          ? Color(0xFF212121)
-                          : Colors.black;
-                    }),
-                  ),
-                  controller: searchController,
-                  cursorColor: Colors.pink,
-                  decoration: InputDecoration(
-                    isDense: true,
-                    contentPadding: EdgeInsets.fromLTRB(12, 12, 12, 12),
-                    focusColor: Colors.red,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        style: BorderStyle.solid,
-                        color: Colors.red,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(0)),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        style: BorderStyle.solid,
-                        color: Colors.transparent,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(0)),
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                        style: BorderStyle.solid,
-                        color: Colors.transparent,
-                      ),
-                      borderRadius: BorderRadius.all(Radius.circular(0)),
-                    ),
-                    hintText: '使用 ${getActive().name} 进行搜索',
-                    hintStyle: TextStyle(color: Colors.white54),
-                  ),
-                  onSubmitted: (value) {
-                    print(value);
-                    focusNode.requestFocus();
-                  },
-                ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              padding: EdgeInsets.all(0),
+              decoration: BoxDecoration(
+                border: Border.all(),
+                color: Color.fromARGB(40, 0, 0, 0),
               ),
-              MouseRegion(
-                cursor: SystemMouseCursors.click,
-                child: InkWell(
-                  child: AnimatedSwitcher(
-                    duration: const Duration(milliseconds: 270),
-                    switchOutCurve: Curves.easeInOut,
-                    switchInCurve: Curves.easeInOut,
-                    transitionBuilder:
-                        (Widget child, Animation<double> animation) {
-                          return FadeTransition(
-                            opacity: animation,
-                            child: child,
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 420,
+                    child: TextField(
+                      focusNode: focusNode,
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: WidgetStateColor.resolveWith((states) {
+                          return states.contains(WidgetState.hovered)
+                              ? Color(0xFF212121)
+                              : Colors.black;
+                        }),
+                      ),
+                      controller: searchController,
+                      cursorColor: Colors.pink,
+                      decoration: InputDecoration(
+                        isDense: true,
+                        contentPadding: EdgeInsets.fromLTRB(12, 12, 12, 12),
+                        focusColor: Colors.red,
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            style: BorderStyle.solid,
+                            color: Colors.red,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(0)),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            style: BorderStyle.solid,
+                            color: Colors.transparent,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(0)),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            style: BorderStyle.solid,
+                            color: Colors.transparent,
+                          ),
+                          borderRadius: BorderRadius.all(Radius.circular(0)),
+                        ),
+                        hintText: '使用 ${getActive().name} 进行搜索',
+                        hintStyle: TextStyle(color: Colors.white54),
+                      ),
+                      onSubmitted: (value) {
+                        doSearch();
+                        focusNode.requestFocus();
+                      },
+                    ),
+                  ),
+                  MouseRegion(
+                    cursor: SystemMouseCursors.click,
+                    child: InkWell(
+                      child: AnimatedSwitcher(
+                        duration: const Duration(milliseconds: 270),
+                        switchOutCurve: Curves.easeInOut,
+                        switchInCurve: Curves.easeInOut,
+                        transitionBuilder:
+                            (Widget child, Animation<double> animation) {
+                              return FadeTransition(
+                                opacity: animation,
+                                child: child,
+                              );
+                            },
+                        layoutBuilder: (currentChild, previousChildren) {
+                          return SizedBox(
+                            width: 20,
+                            height: 20,
+                            child: Stack(
+                              alignment: Alignment.center,
+                              children: <Widget>[
+                                ...previousChildren,
+                                if (currentChild != null) currentChild,
+                              ],
+                            ),
                           );
                         },
-                    layoutBuilder: (currentChild, previousChildren) {
-                      return SizedBox(
-                        width: 20,
-                        height: 20,
-                        child: Stack(
-                          alignment: Alignment.center,
-                          children: <Widget>[
-                            ...previousChildren,
-                            if (currentChild != null) currentChild,
-                          ],
+                        child: KeyedSubtree(
+                          key: ValueKey(getActive().name),
+                          child: SizedBox(
+                            width: 20,
+                            height: 20,
+                            child: getActive().icon,
+                          ),
                         ),
-                      );
-                    },
-                    child: KeyedSubtree(
-                      key: ValueKey(getActive().name),
-                      child: SizedBox(
-                        width: 20,
-                        height: 20,
-                        child: getActive().icon,
                       ),
+                      onTap: () {
+                        switchAction();
+                      },
                     ),
                   ),
-                  onTap: () {
-                    switchAction();
-                  },
-                ),
+                  SizedBox(width: 12),
+                ],
               ),
-              SizedBox(width: 12),
-            ],
-          ),
+            ),
+            FilledButton(
+              onPressed: () {
+                doSearch();
+              },
+              style: ButtonStyle(
+                padding: WidgetStateProperty.all(
+                  const EdgeInsets.symmetric(vertical: 19, horizontal: 24),
+                ),
+                shape: WidgetStateProperty.all<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(borderRadius: BorderRadius.zero),
+                ),
+                backgroundColor: WidgetStateProperty.all(Colors.black87),
+              ),
+              child: Text('搜索'),
+            ),
+          ],
         ),
       ),
     );
